2803f6af767a04afcffc480d23131fe6425ed254
kousu <kousu@trm.kousu.ca>
2014-07-17 01:17:18 -0400

    Pedagogical comments.

--- bitmap.py
+++ bitmap.py
@@ -4,11 +4,14 @@ kousu, 2014, 2-clause BSD
 
 [see class Bitmap for documentation]
 
+This was written for my own practice, and is laced through with more comments than necessary, for that purpose. Perhaps someone will find it pedagogical.
+
 TODO:
  - [ ] support bitwise ops, so that all bits can be and, or'd, and xor'd en masse
  - [ ] support more reasonable slicing semantics: actually slice part way (this requires then having an optional .base object and a .offset, as ndarray does) 
  - [ ] support sums (__add__()) and appends (__iadd__() or append()); generally try to make this look as much like a list and not an array as possible.
  - [x] figure out how to read wordsize  whatever we feed into self._array; do the numpy dtypes have a "width" field or something?
+   - [ ] weird... np.dtype() and np.int32 (et al.) seem very similar, but one makes objects and the others are types which need to be instantiated before I can query .itemsize. This seems like..cruft. I should go digging.
  - [ ] allow passing a list instead of a size (so Bitmap(5) and Bitmap([False, False, False, False, False]) are identical)
  - Tests:
    - [ ] what happens with different dtypes? what happens with the weird ones like strings? does it just fall over badly?
@@ -19,21 +22,61 @@ TODO:
    - [ ] size = very large
    - [ ]
  - [ ] investigate better integration with np.array(). numpy is fairly flexible about what a, and has a lot of framework in place already. maybe we can define a new np.bool1 dtype?
+   - [ ] While I'm at it: It would be nice to support fancy indexing, beyond just slicing.
  - [ ] do profiling and performance testing
    - [ ] does changing the dtype affect speed? how?
+ - [ ] implement on ctypes (e.g. define a 'byte' type with bitfields for each entry and use a switch statement (which in python looks like a dictionary lookup or a tedious if-else tree)), which is part of core python, and compare in a) code complexity b) speed
+ - [ ] support parallel writes? there might be some use cases for updating , and doing one read-write cycle has got to be better.
+ - [ ] implement in cython
 """
 
 
 """
-Notes:
+Notes
+======
+
+Bit Manipulation
+----------------
 
 If you are not familiar with bitmaps, you need to know that on all modern machines,
  data can only be manipulated in chunks at a time: bytes (8 bits), words (32 or 64 bits, depending), or larger.
 So, to set and unset bits, we need to construct other numbers which we call (bit)masks using shifts and inversions.
 
-Endianness:
-Since bytes are *opaque* values (ie we never interact with their components--the bits--directly), endianness
- however, if you look at ._array.data, you might need to be aware of endianness, since 4 ot 8 byte integers are not opaque with regard to their component bytes.
+Get out your truth tables, and observe that the following equations hold:
+ x | 1 = 1
+ x | 0 = x
+that is, OR'ing with a 1 forces a bit on, and or'ing with a 0 is a no-op (because, logically, A or B is always true if B is true).
+So to set a bit we OR with a bitstring that is all 0s except for where we want to set it.
+
+To construct such a string, we can use the shift and or operators: one shift per bit and one or operation to put them all together
+e.g. to make a bitstring with only the 7th (NB: counting from 0) bit set, do 1 << 7, which is 0b1 << 7 == 0b10000000
+
+Similarly,
+ x & 1 = x
+ x & 0 = 0
+So to unset a bit we can AND with a string that is all 1s except for 0s where we want the unsetting to happen.
+
+To make such a bitstring, we first make a mirror image bitstring as above and then *invert*:
+ ~(1<<7) == ~(0b10000000) == 0b01111111
+
+References:
+- https://wiki.python.org/moin/BitwiseOperators
+- https://wiki.python.org/moin/BitManipulation
+
+Endianness
+-----------
+Since numbers are *opaque* values (ie we never interact with their components--
+ the bits--directly), the endianness of your machine is not visible to the code below.
+ When it does 1<<b, that means it is interacting with the bth least-significant bit,
+ and wherever that bit may happen to end up is a hardware implementation detail.
+ However, if you look under the hood's hood, at ._array.data[:], and you are using a
+ type larger than int8 (bytes are *always* opaque on all modern machines)--say, int32-- 
+ you could confusingly see each individual word reversed yet their overall ordering the same.
+ That is:
+ On a little-endian machine (which you probably are probably reading this on)
+       the order of bytes is [ 0, 1, 2, 3 ] [ 4, 5, 6, 7 ] [ 8, 9, 10, 11 ] ....
+  but on a big-endian machine,
+       the order will be     [ 3, 2, 1, 0 ] [ 7, 6, 5, 4 ] [ 11, 10, 9, 8 ] ....
 """
 
 
@@ -60,10 +103,10 @@ class Bitmap(object):
     >>> B[99] = True
     IndexError
     """
-    def __init__(self, size, dtype=np.int32):
+    def __init__(self, size, dtype=np.int8):
         """
-        initialize
-        You can choose what
+        size: the [currently fixed] size of the array.
+        dtype: **THIS SHOULD BE TRANSPARENT AND IRRELEVANT**: optionally, choose which numpy type to use for each word size (ie the bitwidth of each entry in the backing numpy array).
         """
         self._size = size #store the original size because it's (wordsize-1)/wordsize times likely that we are not an even multiple and there will be some waste bits which we must make sure to make illegal to access (also, this is slightly faster for very little space)
         
@@ -74,7 +117,6 @@ class Bitmap(object):
         wordsize = dtype(0).nbytes * 8 #8 bits in a byte
         #print("Wordsize:", wordsize) #DEBUG
         
-
         # precache the mask needed to get the within-word address out of the
         # since wordsize is a power of 2, it has a single bit set at bit location _addrbits
         # so one less than it is the number with _addrbits 1-bits in a row
@@ -113,6 +155,11 @@ class Bitmap(object):
 
 
     def __getitem__(self, n):
+        """
+        access the nth bit in the bitmap, or a list containing a slice.
+        
+        returns: a python bool
+        """
         if isinstance(n, slice):
             "hobo support for slicing"
             return [self[i] for i in range(len(self))[n]]
@@ -122,7 +169,13 @@ class Bitmap(object):
         return bool(self._array[a] & (1 << b))
         
     def __setitem__(self, n, v):
-    
+        """
+        write to the nth bit
+        
+        v must be a python bool.
+        
+        Optionally, n can be a slice and v can be an iterable (containing only python bools)
+        """
         if isinstance(n, slice):
             "hobo support for slicing"
             for i,vv in zip(range(len(self))[n], v):
@@ -134,16 +187,9 @@ class Bitmap(object):
         a, b = self._addr(n)        
         
         if v: #turn on bit b
-            # since
-            # x | 1 = 1
-            # x | 0 = x
-            #  the bit we want on, we set to 1 in our mask, and we leave the rest as 0, so then they get passed through
             self._array[a] |= (1<<b)
         else: #turn off bit b
-            # since
-            # x & 1 = x
-            # x & 0 = 0
-            #  the bit we want on, we set to 0 in our mask, and make all the rest 1
+        
             #  this is trickier, because two's complement gets in the way:
             #  in python (and signed C integers! see inv.c!) "~x = -(x+1)"
             #  in C, you can just ignore the type with a cast
@@ -157,6 +203,7 @@ class Bitmap(object):
         #print("__setitem__; ", "[%d,%d] = " % (a,b), bin(self._array[a])) #DEBUG
 
     def __len__(self):
+        "get the current size of the Bitmap"
         return self._size        
 
 
@@ -201,6 +248,8 @@ def test():
     import sys, random
         
     B = Bitmap(int(sys.argv[1]) if len(sys.argv)>1 else 235)
+    print ("There are ", len(B._array), "%s entries backing " % (B._array.dtype.name,), len(B), "bools")
+    
     expected = []
     for i in range(len(B)):
         b = random.choice([True, False])
@@ -225,7 +274,6 @@ def test():
     #print(V)
     fails = [i for i,(v,b) in enumerate(zip(V, bits)) if v != b]
     assert not fails
-    
     
     test_doc_example()
     
